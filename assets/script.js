73062a5975202bf419dae64c3dbe4da0b912c466
jparker1221 <jonparker1221@gmail.com>
2023-03-07 15:27:57 -0500

    updated html and started js

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+// blank array to store search history
+var searchHist = [];
+
+// openweathermap api key
+var openweathermap = fb727f1a6ae3a38f400775ad30afa200;
+
+// api call to openweathermap.org
+api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={fb727f1a6ae3a38f400775ad30afa200};
+
+
+// function to get current weather for selected city
+
+
+
+// function to get 5-day weather forecast
+
+
+
+// function to save search history to local storage
+
+
+
+// function to recall search history from local storage

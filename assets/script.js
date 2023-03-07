adb550a82e4c77dcb2042ea347b68f63d90e8f12
jparker1221 <jonparker1221@gmail.com>
2023-03-07 17:34:03 -0500

    js updates

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,51 @@
 // blank array to store search history
 var searchHist = [];
-
+// blank variable to store city name
+var cityName = "Winston-Salem";
+// variable to get current date
+var currentDay = dayjs().format('dddd, MMM D, YYYY, hh:mm a');
 // openweathermap api key
-var openweathermap = fb727f1a6ae3a38f400775ad30afa200;
+var apiKey = "fb727f1a6ae3a38f400775ad30afa200";
 
 // api call to openweathermap.org
-api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={fb727f1a6ae3a38f400775ad30afa200};
 
 
-// function to get current weather for selected city
+var requestFiveDayForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=imperial`;
 
 
+// function to fetch current weather for selected city
+function currentWeather(){
+    var requestCurrentWeather = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
+    fetch(requestCurrentWeather)
+        .then(function(response) {
+        return response.json();
+        })
+        .then(function(data){
+            document.getElementById('city-temp').textContent = `Current Temperature: ${data.main.temp}`
+            console.log(data);
+            weatherForecast()
+        })
+}
 
-// function to get 5-day weather forecast
+
+// function to fetch 5-day weather forecast for selected city
+function weatherForecast(){
+
+}
 
 
+// function to store search history to local storage
+var storeCity = function(){
 
-// function to save search history to local storage
+}
 
 
+// function to load search history from local storage
+var loadCities = function(){
 
-// function to recall search history from local storage
+}
+
+// event listeners
+
+
+currentWeather()
